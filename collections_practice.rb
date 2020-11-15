73160113408239cde14ae00885b43ec24b2dabf0
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-15 01:42:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
+array.sort
+end
 
+def sort_array_desc(array)
+array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |input|
+    input[2] = "$"
+end
+end
+
+def find_a(array)
+  array.select do |word|
+    word[0] == "a"
+end
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum =+ num
+  end
+end
